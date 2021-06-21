0e3ba0c745bd1287ffd97b7fb3fb07edb018c547
JúDCraide <juliadcraide31@gmail.com>
2021-06-21 15:33:17 -0300

    Componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export function Header() {
-  return <span>header</span>;
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
